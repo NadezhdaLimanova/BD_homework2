df3d1657f0a2fde26545a127e56afe2804b0fe0a
NadezhdaLimanova <127512552+NadezhdaLimanova@users.noreply.github.com>
2023-06-07 16:25:53 +0300

    fixing mistakes.sql

--- insert.sql
+++ insert.sql
@@ -7,13 +7,15 @@ VALUES('indie-rock'), ('rock-n-roll'), ('indie-pop'), ('chillout'), ('hip-hop');
 INSERT INTO albums(album_name, year)
 VALUES ('AM', '20130909'), ( 'Humbug', '20090819'), ('My Type', '20140417'), 
 ('Hit me baby', '20160909'), ('Super extra Gravity', '20051014'), 
-('The Marshall Mathers LP', '20190523'), ('CAFE DEL MAR: THE BEST OF CAF? DEL MAR', '20200319');
+('The Marshall Mathers LP', '20190523'), ('CAFE DEL MAR: THE BEST OF CAFE DEL MAR', '20200319');
 
 INSERT INTO tracks(track_name, album_id, track_time)
-VALUES  ('Stan', 1, '00:06:44'), ('R U Mine?', 2, '00:03:20'), ('My Type', 4, '00:03:26'), 
+VALUES ('Stan', 1, '00:06:44'), ('R U Mine?', 2, '00:03:20'), ('My Type', 4, '00:03:26'), 
 ('The Sign', 5, '00:02:54'), ('Secret Door', 3, '00:03:46'), ('Do I Wanna Know?', 2, '00:04:33'), 
 ('I Need Some Fine Wine And You, You Need To Be Nice', 6, '00:03:33'),
-('Blah Blah Blah', 6, '00:03:00'), ('The Real Slim Shady', 1, '00:04:44'), ('D Votion', 7, '00:07:05');
+('Blah Blah Blah', 6, '00:03:00'), ('The Real Slim Shady', 1, '00:04:44'),
+('D Votion', 7, '00:07:05'), ('Myself', 1, '00:03:00'), ('My own', 2, '00:03:30'), 
+('my', 3, '00:03:01'), ('Oh my god', 2, '00:04:03'), ('Own my', 3, '00:04:30');
 
 INSERT INTO collections (collection_name, year)
 VALUES ('Curtain Call: The Hits', '20051206'), ('Best Of', '20080130'), 
